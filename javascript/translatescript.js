041d6f252d4666be3dff3fbf7ca7b53faf6152bb
RafaelEngel10 <rafaelengelserafim09@gmail.com>
2025-08-22 15:07:48 -0300

    translate button added

---
+++ javascript/translatescript.js
@@ -0,0 +1,5 @@
+let condicionador = true;
+
+function translatePortuguese() {
+    document.getElementById("translate").textContent = condicionador ? "Uma das questões mais desafiadoras do cenário global contemporâneo." : " One of the most challenging issues in the contemporary global scenario.";
+}
