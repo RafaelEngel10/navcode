5c728c544304d945c5dcc13729a444c5fa97c668
RafaelEngel10 <rafaelengelserafim09@gmail.com>
2025-08-22 15:16:30 -0300

    mudanças no js

--- javascript/translatescript.js
+++ javascript/translatescript.js
@@ -1,5 +1,5 @@
 let condicionador = true;
 
 function translatePortuguese() {
-    document.getElementById("translate").textContent = condicionador ? "Uma das questões mais desafiadoras do cenário global contemporâneo." : " One of the most challenging issues in the contemporary global scenario.";
+    document.getElementById("translate").innerHTML = 'Uma das questões <span id="important-span">mais desafiadoras</span> do cenário global contemporâneo.';
 }
