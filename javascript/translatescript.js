a172f1dae04a3bffbd861320ec2f510ee9b8637f
RafaelEngel10 <rafaelengelserafim09@gmail.com>
2025-08-22 15:17:52 -0300

    nada

--- javascript/translatescript.js
+++ javascript/translatescript.js
@@ -1,5 +1,3 @@
-let condicionador = true;
-
 function translatePortuguese() {
     document.getElementById("translate").innerHTML = 'Uma das questões <span id="important-span">mais desafiadoras</span> do cenário global contemporâneo.';
 }
